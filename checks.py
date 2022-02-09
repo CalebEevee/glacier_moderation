c830ae73d1131dfe3051398f8a27be4adf473915
CalebEevee <81588274+CalebEevee@users.noreply.github.com>
2022-02-09 13:36:38 -0800

    [22/02/09] Move blacklist.py to checks

--- checks.py
+++ checks.py
@@ -1,10 +1,18 @@
 import disnake
 from disnake.ext import commands
 
+blacklisted_users = []
+
 
 def LEVEL_A(ctx):
-    return commands.has_role(847994265974997002)
+    return (
+        commands.has_role(847994265974997002)
+        and ctx.message.author.id not in blacklisted_users
+    )
 
 
 def LEVEL_W(ctx):
-    return commands.has_role(845457100582420510)
+    return (
+        commands.has_role(845457100582420510)
+        and ctx.message.author.id not in blacklisted_users
+    )
